430a01d0463dac3978a77b7c4f5a16ca9500ef2a
VitorGs2077 <vitor.diniz20008@gmail.com>
2025-04-02 18:02:49 -0300

    Muitas configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:terapa/cadastro.dart';
+import 'package:terapa/main2.dart';
+// Tom azul: (158, 19, 130, 155)
+// Tom verde: (237, 108, 171, 124)
 void main() {
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
       ),
-      home: MyHomePage(),
+      home: TelaLogin(),
     );
   }
 }
@@ -23,10 +28,62 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
+  get icon => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Color.fromARGB(158, 19, 130, 155), Color.fromARGB(237, 108, 171, 124)],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+          ),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat, color: Colors.white),
+            label: 'Papo Cabeça',
+          ),
+          BottomNavigationBarItem(
+            icon: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => cadastro()),
+                );
+              },
+              child: Icon(Icons.search, color: Colors.white),
+            ),
+            label: 'Pesquisa',
+          ),
+          BottomNavigationBarItem(
+            icon: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TelaLogin()),
+                );
+              },
+              child: Icon(Icons.person, color: Colors.white),
+            ),
+            label: 'Perfil',
+          ),
+          
+        ],
+      ),
+      )
+    );
   }
 }
