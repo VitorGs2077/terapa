d1fb43d85acfb6e8f9794689777b41123219048c
VitorGs2077 <vitor.diniz20008@gmail.com>
2025-04-06 20:57:36 -0300

    pepe8

--- lib/main.dart
+++ lib/main.dart
@@ -47,43 +47,42 @@ class _MyHomePageState extends State<MyHomePage> {
           type: BottomNavigationBarType.fixed,
           backgroundColor: Colors.transparent,
           elevation: 0,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat, color: Colors.white),
-            label: 'Papo Cabeça',
-          ),
-          BottomNavigationBarItem(
-            icon: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => cadastro()),
-                );
-              },
-              child: Icon(Icons.search, color: Colors.white),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.white),
+              label: 'Home',
             ),
-            label: 'Pesquisa',
-          ),
-          BottomNavigationBarItem(
-            icon: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TelaLogin()),
-                );
-              },
-              child: Icon(Icons.person, color: Colors.white),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat, color: Colors.white),
+              label: 'Papo Cabeça',
             ),
-            label: 'Perfil',
-          ),
-          
-        ],
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => cadastro()),
+                  );
+                },
+                child: Icon(Icons.search, color: Colors.white),
+              ),
+              label: 'Pesquisa',
+            ),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaLogin()),
+                  );
+                },
+                child: Icon(Icons.person, color: Colors.white),
+              ),
+              label: 'Perfil',
+            ),
+          ],
+        )
       ),
-      )
     );
   }
 }
