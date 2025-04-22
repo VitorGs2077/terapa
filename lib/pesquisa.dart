29da597d0a6a743b64e2cca2dd5d12bdb8cc01cf
VitorGs2077 <vitor.diniz20008@gmail.com>
2025-04-22 10:52:20 -0300

    pagina pesquisa

---
+++ lib/pesquisa.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:terapa/cadastro.dart';
+import 'package:terapa/login.dart';
+import 'package:terapa/main.dart';
+// Tom azul: (158, 19, 130, 155)
+// Tom verde: (237, 108, 171, 124)
+class Pesquisa extends StatelessWidget {
+  const Pesquisa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+      ),
+      home: TelaLogin(),
+    );
+    }
+  }
+class TelaPesquisa extends StatefulWidget {
+  const TelaPesquisa({super.key});
+
+  @override
+  State<TelaPesquisa> createState() => _TelaPesquisaState();
+}
+class _TelaPesquisaState extends State<TelaPesquisa> {
+  get icon => null;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Directory>(
+      future: getApplicationDocumentsDirectory(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text("Erro ao acessar diretório"));
+        } else if (snapshot.hasData) {
+          final directory = snapshot.data!;
+          final nomePath = '${directory.path}/nome.txt';
+          final file = File(nomePath);
+
+          return FutureBuilder<String>(
+          future: file.readAsString(),
+          builder: (context, nomeSnapshot) {
+            if (nomeSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (nomeSnapshot.hasError || !nomeSnapshot.hasData) {
+              return const Center(child: Text("Erro ao ler nome do usuário"));
+            }
+
+            final nomeUsuario = nomeSnapshot.data!;
+          return Scaffold(
+            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+            appBar: AppBar(
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(158, 19, 130, 155),
+                      Color.fromARGB(237, 108, 171, 124),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+              ),
+              title: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Pesquisa',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  border: InputBorder.none,
+                  icon: Icon(Icons.search, color: Colors.white),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            bottomNavigationBar: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(158, 19, 130, 155),
+                    Color.fromARGB(237, 108, 171, 124)
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MyHomePage()),
+                        );
+                      },
+                      child: Icon(Icons.home, color: Colors.white),
+                    ),
+                    label: "Início",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.chat, color: Colors.white),
+                    label: 'Papo Cabeça',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => cadastro()),
+                        );
+                      },
+                      child: Icon(Icons.search, color: Colors.white),
+                    ),
+                    label: 'Pesquisa',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => TelaLogin()),
+                        );
+                      },
+                      child: Icon(Icons.person, color: Colors.white),
+                    ),
+                    label: nomeUsuario,
+                    ),
+                  ],
+                ),
+              ),
+            );
+                  });
+                }
+                return const Center(child: Text("Diretório não encontrado"));
+              },
+            );
+          }
+        }
