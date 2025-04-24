8b67a113432a5d5ac7d33b3db5cb66edd379d44d
VitorGs2077 <vitor.diniz20008@gmail.com>
2025-04-24 09:19:24 -0300

    manutenção na main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:terapa/cadastro.dart';
 import 'package:terapa/login.dart';
 import 'package:terapa/pesquisa.dart';
+import 'package:terapa/terapeuta.dart';
 // Tom azul: (158, 19, 130, 155)
 // Tom verde: (237, 108, 171, 124)
 void main() {
@@ -33,9 +34,11 @@ class MyHomePage extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHomePage> {
   get icon => null;
-
+  
  @override
   Widget build(BuildContext context) {
+    final currentHeight = MediaQuery.of(context).size.height;
+    final currentWidth = MediaQuery.of(context).size.width;
     return FutureBuilder<Directory>(
       future: getApplicationDocumentsDirectory(),
       builder: (context, snapshot) {
@@ -77,6 +80,67 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  width: currentWidth*0.9,
+                  margin: EdgeInsets.all(10),
+                  child: Center(
+                    child: FractionallySizedBox( 
+                      child: Center(
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                            "Para começar, escolha um profissional encontrado em sua área",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Verdana"
+                              ),
+                            ),
+                          )
+                        )),
+                    ),
+                  )
+                ),
+                Container(
+                  height: currentHeight*0.48,
+                  child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Icon(Icons.person_2_outlined),
+                      title: Text('Terapeuta ${index + 1}'),
+                      subtitle: Text('Rua Canela ${index + 1}'),
+                      trailing: Icon(Icons.arrow_forward, color: Colors.red,),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TelaTerapeuta(),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+                ),
+                SizedBox(height: 30,),
+                Container(
+                  child: ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => TelaPesquisa())
+                      );
+                    }, 
+                    child: Text("Procurando por mais opções? Aperte aqui!"),)
+                )
+              ]
             ),
             bottomNavigationBar: Container(
               decoration: BoxDecoration(
